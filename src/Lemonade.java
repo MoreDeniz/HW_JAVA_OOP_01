b8e75b9a0638d49a71c92991dd4ca7017d890892
Mayya <msalosina@mail.ru>
2023-03-04 19:03:53 +0700

    Lemonade

--- src/Lemonade.java
+++ src/Lemonade.java
@@ -1,2 +1,9 @@
-package PACKAGE_NAME;public class Lemonade {
+/*
+    6. Лимонад
+ */
+
+public class Lemonade extends Drinks{
+    public Lemonade(String name, Double prise, Integer quantity, String unit, Double volume) {
+        super(name, prise, quantity, unit, volume);
+    }
 }
