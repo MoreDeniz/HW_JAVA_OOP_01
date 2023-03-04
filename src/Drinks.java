f900274da7313c52115ac8fa1cfc8ed7c9efc6e9
Mayya <msalosina@mail.ru>
2023-03-04 18:58:44 +0700

    Drinks

--- src/Drinks.java
+++ src/Drinks.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class Drinks {
+/*
+    2. Напитки, содержащие следующие свойства:
+        2.1 Объём
+ */
+
+public class Drinks extends Goods{
+    private Double volume;
+    public Drinks(String name, Double prise, Integer quantity, String unit,
+                     Double volume) {
+        super(name, prise, quantity, unit);
+        this.volume = volume;
+    }
+    @Override
+    public String toString() {
+        return super.toString() + " Объём: \'" + volume + "\'";
+    }
 }
