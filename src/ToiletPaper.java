caef1867b85f19c01019ffb9c49ba2d7ca6cc4fa
Mayya <msalosina@mail.ru>
2023-03-04 19:06:58 +0700

    ToiletPaper

--- src/ToiletPaper.java
+++ src/ToiletPaper.java
@@ -1,2 +1,20 @@
-public class ToiletPaper {
+/*
+    10. Туалетная бумага, содержащее следующее свойство:
+        10.1 Количество слоёв
+ */
+
+public class ToiletPaper extends HygieneItems {
+    private Integer layers;
+    public ToiletPaper(String name, Double prise, Integer quantity, String unit,
+                          Integer numbers, Integer layers) {
+        super(name, prise, quantity, unit, numbers);
+        this.layers = layers;
+    }
+    public Integer getLaiers(){
+        return layers;
+    }
+    @Override
+    public String toString() {
+        return super.toString() + ", Количество слоёв: \'" + getLaiers() + "\'";
+    }
 }
