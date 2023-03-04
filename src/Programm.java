297cd32b531b5e452caa610395ac0b89c1b042ee
Mayya <msalosina@mail.ru>
2023-03-04 19:06:17 +0700

    Program

--- src/Programm.java
+++ src/Programm.java
@@ -1,2 +1,13 @@
-public class Programm {
+/*
+    Реализовать в классе Program, метод выводящий все данные о товаре.
+ */
+
+public class Programm extends Goods{
+
+    public static void show(Goods goods){
+        System.out.println(goods);
+    }
+    static void print(Goods item){
+        System.out.println(item);
+    }
 }
