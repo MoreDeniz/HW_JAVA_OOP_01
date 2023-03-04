4e53f0ed0362597babef95022b73022728bd718b
Mayya <msalosina@mail.ru>
2023-03-04 19:02:44 +0700

    GoodsForKids

--- src/GoodsForKids.java
+++ src/GoodsForKids.java
@@ -1,2 +1,27 @@
-public class GoodsForKids {
+/*
+    4. Детские товары:
+        4.1 Минимальный возраст
+        4.2 Гипоаллергенность
+*/
+
+public class GoodsForKids extends Goods{
+    private Integer minAge;
+    private String hypoallergenic;
+    public GoodsForKids(String name, Double prise, Integer quantity, String unit,
+                           Integer minAge, String hypoallergenic) {
+        super(name, prise, quantity, unit);
+        this.minAge = minAge;
+        this.hypoallergenic = hypoallergenic;
+    }
+    public Integer getMinage(){
+        return minAge;
+    }
+    public String getHipo(){
+        return hypoallergenic;
+    }
+    @Override
+    public String toString() {
+        return super.toString() + ", Минимальный возраст: \'" + getMinage() + "\'," +
+                                  "Гипоаллергенность: \'" + getHipo() + "\'";
+    }
 }
