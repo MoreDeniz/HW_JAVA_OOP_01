5858172f413777097ae4b99b9e7db5a3aba5572d
Mayya <msalosina@mail.ru>
2023-03-04 19:03:22 +0700

    HygieneItems

--- src/HygieneItems.java
+++ src/HygieneItems.java
@@ -1,2 +1,20 @@
-public class HygieneItems {
+/*
+    3. Предметы гигиены, содержащие следующие свойства:
+        3.1 Количество штук в упаковке
+*/
+
+public class HygieneItems extends Goods{
+    private Integer numbers;
+    public HygieneItems(String name, Double prise, Integer quantity, String unit,
+                           Integer numbers) {
+        super(name, prise, quantity, unit);
+        this.numbers = numbers;
+    }
+    public Integer getNumbers(){
+        return numbers;
+    }
+    @Override
+    public String toString() {
+        return super.toString() + ", Количество в упаковке: \'" + getNumbers() + "\'";
+    }
 }
