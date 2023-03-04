adf7a505c8a2e031fc092991c55aa1cc31ef0d9f
Mayya <msalosina@mail.ru>
2023-03-04 18:59:34 +0700

    Food

--- src/Food.java
+++ src/Food.java
@@ -1,2 +1,21 @@
-public class Food {
+/*
+     1. Продукты питания, содержащие следующие свойства:
+        1.1 Срок годности
+ */
+
+public class Food extends Goods{
+    private Integer bestBefore;
+    public Food(String name, Double prise, Integer quantity, String unit,
+                   Integer bestBefore) {
+        super(name, prise, quantity, unit);
+        this.bestBefore = bestBefore;
+    }
+    public Integer getBestBefore(){
+        return bestBefore;
+    }
+    @Override
+    public String toString() {
+        return super.toString() +
+                ", Срок годности: \'" + getBestBefore() + " дней\'";
+    }
 }
