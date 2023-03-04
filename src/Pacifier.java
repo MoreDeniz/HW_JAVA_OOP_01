acc33d333c7b4b8bec411ac62c18fe5d37e45a86
Mayya <msalosina@mail.ru>
2023-03-04 19:05:49 +0700

    Pacifier

--- src/Pacifier.java
+++ src/Pacifier.java
@@ -1,2 +1,10 @@
-public class Pacifier {
+/*
+    12. Соска
+ */
+
+public class Pacifier extends GoodsForKids{
+    public Pacifier(String name, Double prise, Integer quantity, String unit,
+                       Integer minAge, String Hypoallergenic) {
+        super(name, prise, quantity, unit, minAge, Hypoallergenic);
+    }
 }
