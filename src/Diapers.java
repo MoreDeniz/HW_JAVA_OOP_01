78e57384906ce27e42031167c4a565a12b60aa90
Mayya <msalosina@mail.ru>
2023-03-04 18:58:15 +0700

    Diapers

--- src/Diapers.java
+++ src/Diapers.java
@@ -1,2 +1,30 @@
-package PACKAGE_NAME;public class Diapers {
+/*
+    11. Подгузники, содержащие следующее свойства:
+        11.1 Размер: NB, S, M, L, XL, XХL
+        11.2 Минимальный вес
+        11.3 Максимальный вес
+        11.4 Тип
+ */
+
+public class Diapers extends GoodsForKids{
+    private String size;
+    private Double minWeight;
+    private Double maxWeight;
+    private String type;
+    public Diapers(String name, Double prise, Integer quantity, String unit,
+                    Integer minAge, String Hypoallergenic,
+                    String size, Double minWeight, Double maxWeight, String type) {
+        super(name, prise, quantity, unit, minAge, Hypoallergenic);
+        this.size = size;
+        this.minWeight = minWeight;
+        this.maxWeight = maxWeight;
+        this.type = type;
+    }
+    @Override
+    public String toString() {
+        return super.toString() + ", Размер: \'" + size +
+                "\', Минимальный вес: \'" + minWeight +
+                "\', Максимальный вес: \'" + minWeight +
+                "\' Тип: \'" + type + "\'";
+    }
 }
