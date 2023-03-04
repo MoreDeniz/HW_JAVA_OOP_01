12a4915d064f9e7b3b60404243c1bae403fa68ea
Mayya <msalosina@mail.ru>
2023-03-04 18:59:10 +0700

    Eggs

--- src/Eggs.java
+++ src/Eggs.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class Eggs {
+/*
+    8. Яйца, содержащее следующиее свойство:
+        8.1 Количество в упаковке
+ */
+
+public class Eggs extends Food{
+    private Integer numbs;
+    public Eggs(String name, Double prise, Integer quantity, String unit,
+                 Integer bestBefore, Integer numbs) {
+        super(name, prise, quantity, unit, bestBefore);
+        this.numbs = numbs;
+    }
+    public Integer getNum(){
+        return numbs;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", Количество в упаковке: \'" + getNum() + "\'";
+    }
 }
