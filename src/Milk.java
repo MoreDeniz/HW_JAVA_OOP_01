e622f57a830e6c25268bf66724f6bc24af47f48d
Mayya <msalosina@mail.ru>
2023-03-04 19:05:11 +0700

    Milk

--- src/Milk.java
+++ src/Milk.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class Milk {
+/*
+    5. Молоко, содержащиее следующее свойство:
+        5.1 Процент жирности
+        5.2 Срок годности
+ */
+
+public class Milk extends Drinks{
+    private Double fat;
+    private Integer bestBefore;
+    public Milk(String name, Double prise, Integer quantity, String unit,
+                 Double volume, Double fat, Integer bestBefore) {
+        super(name, prise, quantity, unit, volume);
+        this.fat = fat;
+        this.bestBefore = bestBefore;
+    }
+    @Override
+    public String toString() {
+        return super.toString() + ", Процент жирности: \'" + fat +
+                "%\', Срок годности: \'" + bestBefore + " суток\'";
+    }
 }
