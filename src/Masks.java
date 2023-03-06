5d56c885fe99df319cb4bd3ff64712567820a106
Mayya <msalosina@mail.ru>
2023-03-06 09:20:25 +0700

    Masks

--- src/Masks.java
+++ src/Masks.java
@@ -7,6 +7,4 @@ public class Masks extends HygieneItems{
     public Masks(String name, Double prise, Integer quantity, String unit, Integer numbers) {
         super(name, prise, quantity, unit, numbers);
     }
-
-
 }
